bb0de788e186e83d5b73a0913e7de9a005c99678
TaufiqOM <pt.om.taufiq@gmail.com>
2025-10-14 15:19:39 +0700

    Pre Shipping v1

---
+++ ITCustom_ShippingReport/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'ITCustom Shipping Report',
+    'version': '1.0',
+    'summary': 'Add Pre Shipping report functionality to Batch Transfers',
+    'description': '''
+        This module adds Pre Shipping report capabilities to Batch Transfers.
+        - Adds print button for Pre Shipping report
+        - Integrates with existing stock_picking_batch module
+    ''',
+    'author': 'ITCustom',
+    'website': 'https://www.itcustom.my.id/',
+    'license': 'LGPL-3',
+    'depends': ['stock_picking_batch'],
+    'data': [
+        'reports/pre_shipping_report.xml',
+        'reports/report_actions.xml',
+        'views/stock_picking_batch_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
